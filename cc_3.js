58245c2667ddb118dd1f546bf118811c324fecff
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-02 15:48:46 -0500

    Task 2- Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -4,3 +4,10 @@ let prices = [5,6,7,8,9];  //Define an array of prices
 prices.push(10); // Added the price 10 to the end of the array
 prices.shift(5); // Removed first value of prices list 
 console.log(prices) // Output [6,7,8,9,10]
+
+//Task 2 Modifiying Customer Orders Scenario 
+
+let orders = [10,11,12,13,14]; //Define an array of order quantities
+orders[2] = 17; // Adding 5 to the third order amount
+let total = orders.reduce((sum,orders) => sum + orders); //Adding the amount of orders together
+console.log(total); //Output: 65
