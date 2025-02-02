6e8aa159e8337611cc16d6dcb947df2be08bcee7
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-02 15:28:13 -0500

    Task 1 Product Price Management

--- cc_3.js
+++ cc_3.js
@@ -3,4 +3,4 @@
 let prices = [5,6,7,8,9];  //Define an array of prices 
 prices.push(10); // Added the price 10 to the end of the array
 prices.shift(5); // Removed first value of prices list 
-console.log(prices) // Output [6,7,8,9,20]
+console.log(prices) // Output [6,7,8,9,10]
